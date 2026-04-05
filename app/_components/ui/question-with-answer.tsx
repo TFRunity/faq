de5043106e69b7d848ffda1c34fc6ede4aeb2bf0
AlexeyBabanov <alexeydustfg@gmail.com>
2026-04-06 02:32:44 +0300

    UI 5/10

--- app/_components/ui/question-with-answer.tsx
+++ app/_components/ui/question-with-answer.tsx
@@ -3,6 +3,10 @@ import React, {useState} from "react";
 import Image from "next/image";
 import {createPortal} from "react-dom";
 import ModalUpdateFaq from "@/app/_components/ui-with-logic/modal-update-faq";
+import "@/app/global-styles.css"
+import {create} from "node:domain";
+import {ModalDeleteQuestion} from "@/app/_components/ui-with-logic/modal-delete-question";
+
 
 
 export interface QuestionWithAnswerProps {
@@ -14,10 +18,12 @@ export default function Question({questionWithAnswer, permission} : QuestionWith
 
     const [toggle, setToggle] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    const [showModalDelete, setShowModalDelete] = useState(false);
 
     function toggleAnswer() {
         setToggle(!toggle);
     }
+
     function openModalChange() {
         setShowModal(true);
     }
@@ -25,23 +31,40 @@ export default function Question({questionWithAnswer, permission} : QuestionWith
         setShowModal(false);
     }
 
+    function openModalDelete() {
+        setShowModalDelete(true);
+    }
+    function closeModalDelete() {
+        setShowModalDelete(false);
+    }
+
     return (
         <div className='flex flex-col columns-10'>
             <div className='m-1'>
                 <div className='rounded-lg bg-slate-100 flex justify-between'>
                     <h3 className=' text-slate-900 text-[120%] mt-3 ml-3 mb-2'>{questionWithAnswer.question.question ? questionWithAnswer.question.question : "Пустой вопрос"}</h3>
-                    <div className=' text-slate-600 text-[130%] cursor-pointer mt-3 mr-3 mb-2' onClick={toggleAnswer}>
+                    <div className=' text-slate-600 text-[130%] cursor-pointer mt-3 mr-3 mb-2'>
                         {permission &&
-                            <div onClick={openModalChange} className='cursor-pointer content-end ml-auto mr-7 flex-row'>
-                                <Image src={"/icons/pencil.png"} width='30' height='30' alt={"Изменить"}
-                                       loading="lazy"/>
-                            </div>
+                            <>
+                                <div onClick={openModalChange} className='cursor-pointer content-end ml-auto mr-7 flex-row'>
+                                    <Image src={"/icons/pencil.png"} width='30' height='30' alt={"Изменить"}
+                                           loading="lazy"/>
+                                </div>
+                                <div onClick={openModalDelete} className='cursor-pointer content-end ml-auto mr-7 flex-row'>
+                                    <Image src={"/icons/close.png"} width='30' height='30' alt={"X"}
+                                           loading="lazy"/>
+                                </div>
+                            </>
                         }
-                        <h2>+</h2>
+                        <h2 onClick={toggleAnswer}>+</h2>
                     </div>
                 </div>
                 {showModal && createPortal(
                     <ModalUpdateFaq questionWithAnswer={questionWithAnswer} exitAction={closeModalChange} />,
+                    document.body
+                )}
+                {showModalDelete && createPortal(
+                    <ModalDeleteQuestion question={questionWithAnswer.question} exitAction={closeModalDelete} />,
                     document.body
                 )}
             </div>
