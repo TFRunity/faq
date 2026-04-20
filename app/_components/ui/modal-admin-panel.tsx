9daa1b3dee76b76851f74431ac8bfab43f5e7033
Vicktoria <vicky4ernysheva@yandex.ru>
2026-04-20 14:47:47 +0300

    UI 9.9/10

--- app/_components/ui/modal-admin-panel.tsx
+++ app/_components/ui/modal-admin-panel.tsx
@@ -33,20 +33,20 @@ export default function AdminPanel({givePermissionsAction, exitAction} : AdminPa
 
     return (
         <div className="modal-bg">
-            <div className="modal-body">
+            <div className="modal-body flex flex-col gap-1">
                 <div className="text-lg text-slate-800 mb-7 modal-header flex justify-between">
-                    <h3>Админ-панель</h3>
-                    <div className='cursor-pointer mt-1' onClick={exitAction}>
+                    <h3 className='p-2'>Админ-панель</h3>
+                    <div className='cursor-pointer hover:bg-slate-200 md:p-2 rounded-md transition duration-200' onClick={exitAction}>
                         <Image src='/icons/close.png' width='24' height='24' alt='close' />
                     </div>
                 </div>
-                <div className='w-1 bg-gray-300 cursor-col-resize'></div>
-                <div className='modal-content flex flex-col gap-3'>
-                    <input className='border border-slate-400 rounded-md p-1' type='text' defaultValue=''
+                <div className="border-t-4 rounded-2xl w-auto border-gray-400 md:m-2"></div>
+                <div className='modal-content flex flex-col gap-3 mt-2'>
+                <input className='border border-slate-400 rounded-md p-1' placeholder='Логин' type='text' defaultValue=''
                            ref={nameInputRef}/>
-                    <input className='border border-slate-400 rounded-md p-1' type='text' defaultValue=''
+                    <input className='border border-slate-400 rounded-md p-1' placeholder='Пароль' type='text' defaultValue=''
                            ref={passwordInputRef}/>
-                    <button className='cursor-pointer mt-3 bg-slate-100 p-2' onClick={logIn}>Войти</button>
+                    <button className='cursor-pointer mt-3 bg-slate-100 p-2 hover:bg-slate-200 transition duration-200' onClick={logIn}>Войти</button>
                 </div>
             </div>
         </div>
